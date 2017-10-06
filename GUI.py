430e04197870cc284746a7ba255e7dac5ae4f86a
thatSteveFan <pramukh.naduthota@gmail.com>
2017-10-05 17:06:27 -0700

    simple app finished

--- GUI.py
+++ GUI.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter.ttk import *
+from socket import *
+import threading
 class GUI:
+		
 	def __init__(self, connection):
 		self_conn = connection
 		self._root = tk.Tk()
@@ -8,14 +11,25 @@ class GUI:
 		
 		text = tk.Text(self._root)
 		text.pack(expand=tk.YES, fill=tk.BOTH)
-		text.insert(tk.END, "initialized")
-		#text.config(state = tk.DISABLED)
+		text.insert(tk.END, "initialized\n")
+		text.config(state = tk.DISABLED)
 		
 		entry = MultilineEntry(self._root)
-		entry.bind("<Control-Return>", lambda x: text.insert(tk.END, x.widget.clear_and_get()))
+		
+		def sendhandler(event):
+			s.send(event.widget.clear_and_get().encode())
+		def rcvhandler(str):
+			print("handling", str)
+			text.config(state = tk.NORMAL)
+			text.insert(tk.END, str)
+			text.config(state = tk.DISABLED)
+		self.rcv_handler = rcvhandler
+		entry.bind("<Control-Return>", sendhandler)
 		entry.pack(expand=tk.YES, fill=tk.BOTH)
 		entry.bind()
 		
+		
+	def start(self):
 		self._root.mainloop()
 		
 class MultilineEntry(tk.Text):
@@ -29,7 +43,23 @@ class MultilineEntry(tk.Text):
 		self.delete("0.0", tk.END)
 		return text
 	
-		
+class ReadThread(threading.Thread):
+	def __init__(self, conn, callback):
+		super().__init__(name = "reader")
+		self.conn = conn
+		self.callback = callback
+	def run(self):
+		print("Starting")
+		while(1):
+			print("started")
+			input = self.conn.recv(4096).decode()
+			print(input)
+			self.callback(input)
 		
 if __name__ == "__main__":
-	gui = GUI(None)
+	s = socket()
+	s.connect(("localhost", 50000))
+	gui = GUI(s)
+	rt = ReadThread(s, gui.rcv_handler)
+	rt.start()
+	gui.start()
--- ServerSide.py
+++ ServerSide.py
@@ -1,10 +1,11 @@
 from socket import *
 from threading import *
-
+from queue import *
 
 class InputThread(Thread):
-	def __init__(self, socket):
+	def __init__(self, socket, queue_to_push_to):
 		super(InputThread, self).__init__(name = "Input" + str(socket.getsockname()))
+		self._queue = queue_to_push_to
 		self._socket = socket
 		self._running = True;
 		
@@ -13,6 +14,7 @@ class InputThread(Thread):
 			input = (self._socket.recv(1024).decode())
 			if input:
 				print(input)
+				self._queue.put(input)
 			else:
 				self._running = False
 	def stop(self):
@@ -21,28 +23,58 @@ class InputThread(Thread):
 class OutputThread(Thread):
 	def __init__(self, socket):
 		super(OutputThread, self).__init__(name = "Output" + str(socket.getsockname()))
+		self._queue = Queue()
 		self._socket = socket
-		self._running = True;
-		
+		self._running = True
+	
 	def run(self):
 		while(self._running):
-			out = input().encode()
-			_socket.send(out)
+			out = self._queue.get().encode()
+			print(self._socket, out)
+			print(self._socket.send(out))
 	def stop(self):
 		self._running = False
+	def out(self, out):
+		self._queue.put(out)
 		
 		
+class DriverThread(Thread):
+	def __init__(self, in_queue, outthreads):
+		super().__init__(name = "Driver")
+		self.queue = in_queue
+		self.outs = outthreads
+		self.running = True
+	def run(self):
+		while(self.running):
+			out = self.queue.get();
+			
+			for thread in self.outs:
+				print(thread, out)
+				thread.out(out)
+		
+	def stop(self):
+		self.running = False
 if __name__ == "__main__":
-	print (input())	
+	
 	s = socket(AF_INET, SOCK_STREAM)
 	s.bind( ("0.0.0.0", 50000) )
 	
 	s.listen()
-	print(s)
-	(conn, adress) = s.accept()
-	it = InputThread(conn)
-	it.start()
-	out = OutputThread(conn)
-	it.join()
+	input_queue = Queue()
 	
+	inthreads = []
+	outthreads = []
+	dt = DriverThread(input_queue, outthreads)
+	dt.start()
+	try:
+		while(1):
+			(conn, adress) = s.accept()
+			it = InputThread(conn, input_queue)
+			it.start()
+			out = OutputThread(conn)
+			outthreads.append(out)
+			out.start()
+	except KeyboardInterrupt:
+		dt.stop()
+		s.close()
 	
